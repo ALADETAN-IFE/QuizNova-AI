f5df91158c2bc357a364d01111f0eabce3687bb3
ALADETAN-IFE <famousfotune123@gmail.com>
2025-04-21 14:37:23 +0100

    debugging quiz page api

--- src/app/api/quizzes/[userId]/routes.ts
+++ src/app/api/quizzes/[userId]/routes.ts
@@ -20,7 +20,7 @@ export async function GET (req: Request)  {
   } catch (error) {
     console.error('Error fetching quizzes:', error);
     return NextResponse.json(
-      { error: 'Failed to fetch quizzes' },
+      { error: 'Failed to fetch quizzes', msg: error },
       { status: 500 }
     );
   }

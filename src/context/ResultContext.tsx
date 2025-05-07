1dd0706b4eb7516283e96fdaedc816a4bca77154
ALADETAN-IFE <famousfotune123@gmail.com>
2025-05-07 03:38:42 +0100

    fixing errors

--- src/context/ResultContext.tsx
+++ src/context/ResultContext.tsx
@@ -100,7 +100,7 @@ interface ResultContextType {
     useEffect(() => {
         const syncLocalResults = async () => {
       // Skip if already synced or no user loggedIn
-      if(!user) setHasSynced(false) return;
+      if(!user) return setHasSynced(false);
       if (hasSynced) return;
       // if (!user || hasSynced) return;
       console.log("hasSynced", hasSynced)

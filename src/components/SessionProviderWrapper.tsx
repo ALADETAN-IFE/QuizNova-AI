f4c23707e216a94e6f1aa21c8193ec64e7bf854a
ALADETAN-IFE <famousfotune123@gmail.com>
2025-05-10 18:12:50 +0100

    defining typoe of children for the sessionproviderWrapper

--- src/components/SessionProviderWrapper.tsx
+++ src/components/SessionProviderWrapper.tsx
@@ -2,6 +2,9 @@
 
 import { SessionProvider } from 'next-auth/react'
 
-export const SessionProviderWrapper = ({ children }) => {
+export const SessionProviderWrapper = ({   children,
+}: {
+  children: React.ReactNode
+}) => {
   return <SessionProvider>{children}</SessionProvider>
 }
